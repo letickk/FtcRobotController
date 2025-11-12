87d4f9571e55dff986996ad1e39a41b4c9025ec2
letickk <ame.prime@mail.ru>
2025-11-12 14:19:46 +0600

    Create Training.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Training {
+    int robotHeight
+    double motorSpeed;
+    boolean clawClosed;
+
+    robotHeight  = 10
+    motorSpeed = 0.5
+    clawClosed = true;
+}
